420132ae40347d2d2b4b5d5ad8c8758a975eef67
HaydenA0 <aregrag2005@gmail.com>
2025-12-09 04:05:43 +0100

    got some requests going

--- src/main.py
+++ src/main.py
@@ -1,1 +1,20 @@
-print("hello world")
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def main():
+    return flask.render_template("index.html")
+
+
+@app.route("/", methods=["POST"])
+def predict():
+    image_file = flask.request.files["imagefile"]
+    image_path = "./images/" + str(image_file.filename)
+    image_file.save(image_path)
+    return flask.render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(port=3000, debug=True)
